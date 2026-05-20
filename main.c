09f3e37c462cfbdd789d4d0f667ebd497f6f7dd6
Matias Shuguli <matias.shuguli@udla.edu.ec>
2026-05-20 15:06:39 -0500

    Cuarto commit - por referencia

--- main.c
+++ main.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 
-float calculoPromedio(float notas[5]) {
+void calculoPromedioReferencia(float notas[5], float *resultado) {
     float suma = 0;
     for (int i = 0; i < 5; i++) {
         printf("notas[%d] %.1f\n", i, notas[i]);
         suma += notas[i];
     }
-    return suma / 5.0f;
+    
+    *resultado = suma / 5.0f;
 }
 
 int main(int argc, char *argv[]) {
     float notas[5] = {6.5, 7.1, 8, 6, 5};
-    printf("promedio: %.2f\n", calculoPromedio(notas));
+    float promedio = 0;
+
+    calculoPromedioReferencia(notas, &promedio);
+    printf("promedio: %.2f\n", promedio);
     return 0;
 }
