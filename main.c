b9439cd51d6ea3b2abdba4c68b208e8af5afcb5c
Matias Shuguli <matias.shuguli@udla.edu.ec>
2026-05-20 15:03:16 -0500

    Tercer commit-Crear funcion por valor

--- main.c
+++ main.c
@@ -1,21 +1,16 @@
 #include <stdio.h>
 
-int main (int argc, char *argv[]) {
-    float notas[5] = {6.5, 7.1, 8, 6, 5};
-    float promedio = 0; 
+float calculoPromedio(float notas[5]) {
     float suma = 0;
-    
-    for (int i = 0; i < 5; i++)
-    {
+    for (int i = 0; i < 5; i++) {
         printf("notas[%d] %.1f\n", i, notas[i]);
         suma += notas[i];
     }
-    
-    
-    promedio = suma / 5.0f; 
-    
-    
-    printf("promedio: %.2f\n", promedio); 
-    
+    return suma / 5.0f;
+}
+
+int main(int argc, char *argv[]) {
+    float notas[5] = {6.5, 7.1, 8, 6, 5};
+    printf("promedio: %.2f\n", calculoPromedio(notas));
     return 0;
 }
