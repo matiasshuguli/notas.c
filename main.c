a6cb1d8faa937e68e91d57339b616bc3cce096d7
Matias Shuguli <matias.shuguli@udla.edu.ec>
2026-05-20 14:42:24 -0500

    Segundo commit

--- main.c
+++ main.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 
 int main (int argc, char *argv[]) {
-    float notas[5] = {6.5,7.1,8,6,5};
+    float notas[5] = {6.5, 7.1, 8, 6, 5};
+    float promedio = 0; 
+    float suma = 0;
+    
     for (int i = 0; i < 5; i++)
     {
-        printf("notas[%d] %.1f\n",i, notas[i]);
+        printf("notas[%d] %.1f\n", i, notas[i]);
+        suma += notas[i];
     }
+    
+    
+    promedio = suma / 5.0f; 
+    
+    
+    printf("promedio: %.2f\n", promedio); 
     
     return 0;
 }
