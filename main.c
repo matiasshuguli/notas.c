5d9dca0955aef960de56bc29fa508f0b603127db
Matias Shuguli <matias.shuguli@udla.edu.ec>
2026-05-20 15:18:21 -0500

    Quinto commit, arreglo

--- main.c
+++ main.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 
-void calculoPromedioReferencia(float notas[5], float *resultado) {
-    float suma = 0;
-    for (int i = 0; i < 5; i++) {
-        printf("notas[%d] %.1f\n", i, notas[i]);
-        suma += notas[i];
-    }
-    
-    *resultado = suma / 5.0f;
-}
+float calcularPromedioxRef(float *ptr);
+int main (int argc, char *argv[]) {
 
-int main(int argc, char *argv[]) {
-    float notas[5] = {6.5, 7.1, 8, 6, 5};
-    float promedio = 0;
-
-    calculoPromedioReferencia(notas, &promedio);
-    printf("promedio: %.2f\n", promedio);
-    return 0;
+   float notas[5] = {6.5, 7.1, 8, 6, 5};
+   printf("promedio: %.2f\n", calcularPromedioxRef(notas));
+   return 0;
 }
+ 
+
+
+float calcularPromedioxRef(float *ptr) {
+
+   float suma = 0; 
+   for (int i = 0; i < 5; i++) {
+       printf("notas[%d] %.1f\n", i, ptr[i]); 
+       suma += ptr[i];
+
+   }
+   float promedio = suma / 5.0f;
+   return promedio;
+
+}
+ 
